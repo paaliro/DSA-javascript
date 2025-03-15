c53ef47ccfff25cb8ad4f91b0ca6bfa62e22a409
Parveen Kumar <123547083+paaliro@users.noreply.github.com>
2025-03-15 13:52:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,92 @@
+
+// Question No.09
+// let amount = Number(prompt("What is the Final Amount ?"))
+// let discount = 0;
+
+// if (amount > 0 && amount <= 5000) {
+//     discount = 0;
+// } else if (amount > 5000 && amount <= 7000) {
+//     discount = 5;
+// } else if (amount > 7000 && amount <= 9000) {
+//     discount = 10;
+// } else if (amount > 9000) {
+//     discount = 20;
+// } console.log("Payable amount:", amount - Math.floor((discount * amount) / 100));
+
+
+
+// Question No.11
+
+// let unit = Number(prompt("Enter electricity unit ?"))
+// let amount = 0;
+
+// if (unit > 400) {
+//     amount = (unit - 400) * 13;
+//     unit = 400;
+// }
+// if (unit > 200 && unit <= 400) {
+//     amount += (unit - 200) * 8;
+//     unit = 200;
+// }
+// if (unit > 100 && unit <= 200){
+//     amount += (unit - 100) * 6;
+//     unit = 100;
+// }
+// amount += unit * 4;
+// console.log("Total amount to be paid:", amount);
+
+
+// 09.05.2025
+
+// Question No.12 
+
+// let amount = 790;
+
+// if (amount >= 500) {
+//     console.log("500 notes :", Math.floor(amount/500));
+//     amount = amount % 500;   
+    
+// }
+// if (amount >= 200) {
+//     console.log("200 notes :", Math.floor(amount/200));
+//     amount = amount % 200;   
+    
+// }
+// if (amount >= 100) {
+//     console.log("100 notes :", Math.floor(amount/100));
+//     amount = amount % 100;   
+    
+// }
+// if (amount >= 50) {
+//     console.log("50 notes :", Math.floor(amount/50));
+//     amount = amount % 50;   
+    
+// }
+// if (amount >= 20) {
+//     console.log("20 notes :", Math.floor(amount/20));
+//     amount = amount % 20;   
+    
+// }
+// if (amount >= 10) {
+//     console.log("10 notes :", Math.floor(amount/10));
+//     amount = amount % 10;   
+    
+// }
+// if (amount >= 5) {
+//     console.log("5 coins :", Math.floor(amount/5));
+//     amount = amount % 5;   
+    
+// }
+// if (amount >= 2) {
+//     console.log("2 coins :", Math.floor(amount/2));
+//     amount = amount % 2;   
+    
+// }
+// if (amount === 1) {
+//     console.log("1 coins :", amount);
+// }
+
+// Ternary Operator
+// ?(condition): true : false
+
+// Nested ternary operator 
